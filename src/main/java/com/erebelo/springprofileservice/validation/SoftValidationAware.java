ae55dcdb6535f7af2027452b22ed543c2820c836
erebelo <eduldrebelo@gmail.com>
2026-09-09 15:28:51 -0300

    Adding comments to SoftValidationAware interface

--- src/main/java/com/erebelo/springprofileservice/validation/SoftValidationAware.java
+++ src/main/java/com/erebelo/springprofileservice/validation/SoftValidationAware.java
@@ -2,6 +2,11 @@ package com.erebelo.springprofileservice.validation;
 
 import java.util.List;
 
+/**
+ * Defines the contract for objects that support soft validation by exposing a
+ * {@code softValidationFailures} property that can be populated with validation
+ * failures.
+ */
 public interface SoftValidationAware {
 
     void setSoftValidationFailures(List<SoftValidationFailure> failures);
